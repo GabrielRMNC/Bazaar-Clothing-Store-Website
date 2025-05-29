9678b07614ec639bf198ba92bde7c48e0a285d03
GabrielRMNC <romanicagabrielmail@gmail.com>
2025-05-29 14:28:05 +0300

    Deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'bazaar-clothing-store-website-production.up.railway.app';
 
 export const clothingApi = {
     async getAllClothes(page = 1, limit = 8, category, brand, sort) {
